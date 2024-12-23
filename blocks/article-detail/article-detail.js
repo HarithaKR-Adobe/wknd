003a885279c29ba1e351e269e8e993880b7c0654
Haritha K R <438943@conneqtcorp.com>
2024-12-23 15:03:33 +0530

    article page added

--- blocks/article-detail/article-detail.js
+++ blocks/article-detail/article-detail.js
@@ -1,7 +1,9 @@
 export default function decorate(block) {
-    const heading = block.querySelector("h2");
-    const subHeading = block.querySelector("h4");
-    const content = block.querySelector("p");
+    const heading = block.children[0].querySelector("h2");
+    const subHeading = block.children[0].querySelector("h4");
+    const content = block.children[1].querySelector("p");
+    const imgBlock = block.children[2];
+
     block.innerHTML = `
         <div class="article-heading">
             <h2>${heading.innerText}</h2>
@@ -10,5 +12,26 @@ export default function decorate(block) {
         <div class="article-content">
             ${content.innerText}
         </div>
+        <figure></figure>
     `;
+
+    const figure = block.querySelector("figure");
+
+    if (imgBlock) {
+        const img = imgBlock.querySelector("img");
+        const caption = imgBlock.querySelector("p");
+
+        if (img) {
+            figure.appendChild(img);
+        }
+        if (caption) {
+            const figCaption = document.createElement("figcaption");
+            figCaption.innerHTML = caption.innerText;
+            figure.appendChild(figCaption);
+        }
+
+    } else {
+        figure.remove();
+    }
+
 }
